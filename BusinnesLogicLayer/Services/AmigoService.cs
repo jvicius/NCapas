a3fd4ec44189de383a378714396a149de38da7c3
Jose Velarde <jvicius@gmail.com>
2019-09-12 12:46:13 -0500

    NCapas

---
+++ BusinnesLogicLayer/Services/AmigoService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using DataAccessLayer;
+using DomainLayer.Models;
+using System.Collections.Generic;
+
+namespace BusinnesLogicLayer.Services
+{
+    public class AmigoService
+    {
+        private readonly AmigoDataService _dataService;
+
+        public AmigoService(string connectionString)
+        {
+            _dataService = new AmigoDataService(connectionString);
+        }
+
+        public List<Amigo> GetAmigos()
+        {
+            return _dataService.GetAmigos();
+        }
+
+        public string AddAmigo(Amigo amigo)
+        {
+            try
+            {
+                return _dataService.AddAmigo(amigo) ? "Amigo Add Successfully" : "Error Adding Amigo";
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+            }
+        }
+
+        public string DeleteAmigo(int idamigo)
+        {
+            try
+            {
+                return _dataService.DeleteAmigo(idamigo) ? "Amigo Delete Successfully" : "Error in delete Amigo";
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+            }
+        }
+    }
+}
---
+++ EjemploNCapas/sql/InitDb.sql
@@ -0,0 +1,61 @@
+﻿create database agenda
+go
+use agenda
+go
+
+create table amigos(
+idamigo int primary key identity(1,1),
+nombre nvarchar(50) not null,
+fecnac datetime,
+direccion nvarchar(100),
+telefono nvarchar(50));
+go
+
+insert into amigos
+values
+('JVelarde','04/13/1982','San Antonio 101-19','4491091867')
+go
+
+create procedure getamigos
+as
+select * from amigos
+go
+
+create procedure addamigo
+(
+	@nombre nvarchar(50),
+	@fecnac datetime,
+	@direccion nvarchar(100),
+	@telefono nvarchar(50),
+	@haserror bit out
+)
+as
+begin try
+	set @haserror = 0;
+	insert into amigos
+	values
+	(@nombre,@fecnac,@direccion,@telefono)
+end try
+begin catch
+	set @haserror = 1;
+end catch
+go
+
+create procedure deleteamigo
+(
+	@idamigo int,
+	@haserror bit out
+)
+as
+set @haserror = 1
+begin try
+if exists(select top 1 1 from amigos where idamigo = @idamigo)
+begin
+	set @haserror = 0
+	delete amigos where idamigo = @idamigo
+end
+end try
+begin catch
+	set @haserror = 1;
+end catch
+go
